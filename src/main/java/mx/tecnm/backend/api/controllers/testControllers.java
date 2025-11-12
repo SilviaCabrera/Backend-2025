2b72d4cc36bd2d2350ec5008bc71e31b8b84c783
SilviaCabrera <lupitacabrera324@gmail.com>
2025-11-12 02:57:50 +0000

    agregue el testControllers y puse un hola mundo

---
+++ src/main/java/mx/tecnm/backend/api/controllers/testControllers.java
@@ -0,0 +1,16 @@
+package mx.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class testControllers {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hola API Rest";
+    }
+
+}
